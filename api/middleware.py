36ea4d88b43144006a82928eb3e82230605eef59
MathiMartinez00 <mathias.martinez018@gmail.com>
2024-03-08 22:20:45 -0300

    Added exceptions to middleware for incorrect format and invalid users

--- api/middleware.py
+++ api/middleware.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseForbidden
 from django.utils.deprecation import MiddlewareMixin
 from django.contrib.auth import get_user_model
 
@@ -11,9 +12,14 @@ class BasicAuthMiddleware(MiddlewareMixin):
             values = request.headers['Authorization'].split(" ")
             auth_type = values[0]
             token_value = values[1]
-            user = User.objects.filter(username=token_value)
-            if user:
-                request.user = user[0]
+            try:
+                user = User.objects.filter(username=token_value).get()
+                request.user = user
+            except User.DoesNotExist:
+                raise Exception("User does not exist.")
+            if auth_type != "Token":
+                raise Exception("Invalid authorization format.")
+
         return None
 
     def process_response(self, request, response):
