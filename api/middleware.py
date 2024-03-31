9c7ce5e20258df8a636cf32771402e35830dee90
MathiMartinez00 <mathias.martinez018@gmail.com>
2024-03-31 19:36:17 -0400

    Return 401 when Authorization header is invalid

--- api/middleware.py
+++ api/middleware.py
@@ -1,24 +1,23 @@
-from django.http import HttpResponseForbidden
 from django.utils.deprecation import MiddlewareMixin
-from django.contrib.auth import get_user_model
+from api.models import UserToken
+from django.http import HttpResponse
 
 
-# TODO: Figure this out. Add a way to generate tokens.
 class BasicAuthMiddleware(MiddlewareMixin):
-
     def process_request(self, request):
-        User = get_user_model()
         if request.headers.get('Authorization', None):
             values = request.headers['Authorization'].split(" ")
+
             auth_type = values[0]
+            if auth_type != 'Bearer' or len(values) > 2:
+                return HttpResponse('Invalid format.', status=401)
+
             token_value = values[1]
             try:
-                user = User.objects.filter(username=token_value).get()
-                request.user = user
-            except User.DoesNotExist:
-                raise Exception("User does not exist.")
-            if auth_type != "Token":
-                raise Exception("Invalid authorization format.")
+                token = UserToken.objects.filter(bearer_token=token_value).get()
+                request.user = token.user
+            except UserToken.DoesNotExist:
+                return HttpResponse('Invalid token.', status=401)
 
         return None
 
