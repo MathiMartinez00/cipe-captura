a01b74b2bce3177832034f996a4121f56e145e6d
MathiMartinez00 <mathias.martinez018@gmail.com>
2024-06-07 18:28:52 -0400

    Register new models in admin page

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from api.models import UserToken
+from api.models import UserToken, Complaint, ComplaintType, RoadType, City
 
 # Register your models here.
 admin.site.register(UserToken)
+admin.site.register(Complaint)
+admin.site.register(ComplaintType)
+admin.site.register(RoadType)
+admin.site.register(City)
--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 import secrets
 import string
 
@@ -18,32 +19,42 @@ class UserToken(models.Model):
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
-        alphabet = string.ascii_letters + string.digits
-        bearer_token = ''.join(secrets.choice(alphabet) for _ in range(32))
-        UserToken.objects.create(user=instance, bearer_token=bearer_token)
+        Token.objects.create(user=instance)
 
 
 class ComplaintType(models.Model):
     name = models.CharField(max_length=64)
     code = models.CharField(max_length=8)
 
+    def __str__(self):
+        return self.name
+
 
 class City(models.Model):
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
 
 class RoadType(models.Model):
     name = models.CharField(max_length=32)
 
+    def __str__(self):
+        return self.name
+
 
 class Complaint(models.Model):
     complaint_type = models.ForeignKey(ComplaintType, on_delete=models.CASCADE)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
     altitude = models.FloatField()
     accuracy = models.FloatField()
-    photo = models.ImageField(upload_to='complaint_photos/')
+    photo = models.ImageField(upload_to='complaint_photos/', null=True, blank=True)
     road_type = models.ForeignKey(RoadType, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Complaint {self.complaint_type.name}'
