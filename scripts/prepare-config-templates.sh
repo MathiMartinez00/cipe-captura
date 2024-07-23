535da8e1787a3d7e4d6e044b2521703ee3189f74
MathiMartinez00 <mathias.martinez018@gmail.com>
2024-07-22 22:51:33 -0400

    Update docker prod files

--- scripts/prepare-config-templates.sh
+++ scripts/prepare-config-templates.sh
@@ -4,7 +4,7 @@ cp -v .env.prod.db.sample .env.prod.db
 cp -v .env.prod.sample .env.prod
 echo ""
 echo "This is a randomly generated secret key to put in your new .env.prod"
-KEY=$(python -c 'import random; print("".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]))')
+KEY=$(python3 -c 'import random; print("".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]))')
 echo "SECRET_KEY=$KEY"
 echo ""
 echo "Remember to customize your passwords and Google Maps API KEY"
