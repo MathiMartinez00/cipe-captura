61f3461fd8e6d54280cfec513de2073ad658e42e
MathiMartinez00 <mathias.martinez018@gmail.com>
2024-10-26 20:18:24 -0300

    Add generate-random-data invocation in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,5 +16,6 @@ python manage.py migrate
 python manage.py loaddata data/initial_data.json
 python manage.py collectstatic --no-input
 python manage.py createsuperuser --username superuser --email superuser@example.com --no-input
+python manage.py generate-random-data
 
 exec "$@"
