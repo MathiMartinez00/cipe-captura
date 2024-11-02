f36b22f07cc8232f4fc6b18a6d4376b1f72f2e50
MathiMartinez00 <mathias.martinez018@gmail.com>
2024-11-01 22:13:39 -0300

    Starting place, everything is installed

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 # pull official base image
-FROM python:3.12-alpine
+FROM nikolaik/python-nodejs:python3.12-nodejs22-alpine
 
-# set work directory
 WORKDIR /usr/src/app
-
+COPY package-lock.json package.json /usr/src/app/
+RUN npm install
+RUN npm install @rollup/rollup-linux-x64-musl --save-optional
 # set environment variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
@@ -13,7 +14,7 @@ RUN apk --no-cache add --virtual build-dependencies \
     build-base \
     gcc \
     libc-dev \
-    libffi-dev
+    libffi-dev 
 
 # install app dependencies
 RUN pip install --upgrade pip
@@ -25,6 +26,8 @@ COPY ./entrypoint.sh /usr/src/app/entrypoint.sh
 
 # copy project
 COPY ./ /usr/src/app/
+
+# COPY --from=vite ./ /usr/src/app/
 
 # run entrypoint.sh
 ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
--- cipe/settings.py
+++ cipe/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'django_vite_plugin',
 ]
 
 MIDDLEWARE = [
@@ -128,11 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = BASE_DIR / 'static'
 
 GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
-
 
 LOGGING = {
     'version': 1,
