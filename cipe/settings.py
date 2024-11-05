15e5cdcf0672e22407a38430f43399e290306e80
MathiMartinez00 <mathias.martinez018@gmail.com>
2024-11-04 21:19:27 -0300

    Update nginx config

--- cipe/settings.py
+++ cipe/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
 
