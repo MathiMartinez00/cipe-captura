662bb753aec558e73b55554912300513c98b78dd
MathiMartinez00 <mathias.martinez018@gmail.com>
2025-05-05 19:14:56 -0300

    Add django-cors-headers to dependencies

--- cipe/settings.py
+++ cipe/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'corsheaders',
     'drf_spectacular',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -199,3 +201,5 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
     'COMPONENT_SPLIT_REQUEST': True,
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
